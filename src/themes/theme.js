fbc8a13b0e856947e73702087a642fde16b1210c
Peng Wei <mr.pengwei@outlook.com>
2021-03-19 11:49:12 +0800

    add styles

--- src/themes/theme.js
+++ src/themes/theme.js
@@ -1,11 +1,11 @@
-export default {
+export const Theme = {
   fonts: {
     main: "Muli, sans-serif",
     code: "Roboto Mono, monospace",
   },
   colors: {
     main1: "hsl(207, 70%, 59%)",
-    main1: "hsl(207, 70%, 94%)",
+    main2: "hsl(207, 70%, 94%)",
     dark1: "hsl(227, 2%, 12%)",
     dark2: "hsl(227, 2%, 26%)",
     dark3: "hsl(227, 2%, 64%)",
